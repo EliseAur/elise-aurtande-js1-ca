6751f06344bb0be9aedaac5a0e3ba92ddf32d2a1
EliseAur <94222653+EliseAur@users.noreply.github.com>
2022-10-17 23:56:35 +0200

    Update details.js

--- js/details.js
+++ js/details.js
@@ -4,32 +4,19 @@ const params = new URLSearchParams(queryString);
 const id = params.get("id");
 console.log(id);
 
-// const imageContainer = document.querySelector(".container");
-// const queryStringImage = document.location.search;
-// const paramsImage = new URLSearchParams(queryStringImage);
-// const image = paramsImage.get("reference_image_id");
-// console.log(image);
-
 const options = {
     method: "GET",
     headers: {
-        //"x-api-host": "api.thedogapi.com",
-        //"Access-Control-Allow-Origin": "https://api.thedogapi",
         "x-api-key": "live_GqNMBqU0JDhtsGrfh0aCydwIb9LtegmerXbVQA8nKDxXFZDYIYMJWmcZX4jESb1R",
     },
 };
 
 const url = "https://api.thedogapi.com/v1/breeds/" + id;
-//const urlImage = `https://cdn2.thedogapi.com/images/${image}.jpg`;
 
 async function getDetails() {
     const response = await fetch(url);
     const details = await response.json();
     console.log(details);
-
-    // const secondResponse = await fetch(urlImage);
-    // const detailsImage = await secondResponse.json();
-    // console.log(detailsImage);
 
     setTimeout(function () {
         resultsContainer.innerHTML = "";
