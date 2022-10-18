7164ba964d73bfa37404ab9456da465245095b6b
EliseAur <94222653+EliseAur@users.noreply.github.com>
2022-10-18 09:18:14 +0200

    details update

--- js/details.js
+++ js/details.js
@@ -21,51 +21,17 @@ async function getDetails() {
     setTimeout(function () {
         resultsContainer.innerHTML = "";
 
+        const image = details.reference_image_id;
+
         resultsContainer.innerHTML = `<div class="card">
                                                 <h3>${details.name}</h3>
                                                 <li><b>Life span: </b>${details.life_span}</li>
                                                 <li><b>Temperament: </b>${details.temperament}</li>
                                                 <div>
-                                                    <img src="${details.reference_image_id}" alt="${details.name}" />
+                                                    <img src="https://cdn2.thedogapi.com/images/${details.reference_image_id}.jpg" alt="${details.name}" />
                                                 </div>
                                             </div>`;
     }, 2000);
 }
 
 getDetails();
-
-const imageContainer = document.querySelector(".container");
-const queryStringImage = document.location.search;
-const paramsImage = new URLSearchParams(queryStringImage);
-const image = paramsImage.get("reference_image_id");
-console.log(image);
-
-const imageApi = {
-    method: "GET",
-    headers: {
-        //"x-api-host": "api.thedogapi.com",
-        //"Access-Control-Allow-Origin": "https://api.thedogapi",
-        "x-api-key": "live_GqNMBqU0JDhtsGrfh0aCydwIb9LtegmerXbVQA8nKDxXFZDYIYMJWmcZX4jESb1R",
-    },
-};
-
-//const url = "https://api.thedogapi.com/v1/breeds/" + id;
-const urlImage = `https://cdn2.thedogapi.com/images/${image}.jpg`;
-
-async function getImage() {
-    // const response = await fetch(url);
-    // const details = await response.json();
-    // console.log(details);
-
-    const secondResponse = await fetch(urlImage);
-    const detailsImage = await secondResponse.json();
-    console.log(detailsImage);
-
-    setTimeout(function () {
-        imageContainer.innerHTML = "";
-
-        imageContainer.innerHTML = `<img src="${detailsImage}" alt="${details.name}" />`;
-    }, 2000);
-}
-
-getImage();
