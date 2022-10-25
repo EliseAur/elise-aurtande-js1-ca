dace8509093d19f60dd4d79121906d4ca27c3ec0
EliseAur <94222653+EliseAur@users.noreply.github.com>
2022-10-25 20:39:03 +0200

    errorMes and contact

---
+++ js/components/displayError.js
@@ -0,0 +1,16 @@
+// Reusable function for handling errors. 
+
+function displayError(message = "Unknown error") {
+    return `<div class="errorMes card">
+                
+                    <h2>Sorry...</h2>
+                    <p>${message}</p>
+                    <a href="index.html">Try again</a>
+                
+                
+            </div>`;
+}
+
+
+
+{/* <div class="card"></div> */}
--- js/script.js
+++ js/script.js
@@ -14,23 +14,33 @@ const url = "https://api.thedogapi.com/v1/breeds";
 const resultsContainer = document.querySelector(".results");
 
 async function callApi() {
-    const response = await fetch(url);
-    const json = await response.json();
-    console.log(json);
+    try{
+        const response = await fetch(url);
+        const json = await response.json();
+        console.log(json);
+    
+        setTimeout(function () {
+            resultsContainer.innerHTML = "";
+    
+            for (let i = 0; i < json.length; i++) {
+                resultsContainer.innerHTML += `<a href="details.html?id=${json[i].id}"class="card">
+                                                    <h3>${json[i].name}</h3>
+                                                    <li><b>Bred for: </b>${json[i].bred_for}</li>
+                                                    
+                                                    <div class="image" 
+                                                        style="background-image: url('${json[i].image.url}')")></div>
+                                                </a>`;
+            }
+        }, 2500);
+    } catch(error) {
+        setTimeout(function () {
+            console.log("Error when calling the API:", error);
+            resultsContainer.innerHTML = "";
+            resultsContainer.innerHTML += displayError("An error occoured when loading the list of dogs");
+        }, 1500);
+    }
+    }
+    
 
-    setTimeout(function () {
-        resultsContainer.innerHTML = "";
-
-        for (let i = 0; i < json.length; i++) {
-            resultsContainer.innerHTML += `<a href="details.html?id=${json[i].id}"class="card">
-                                                <h3>${json[i].name}</h3>
-                                                <li><b>Bred for: </b>${json[i].bred_for}</li>
-                                                
-                                                <div class="image" 
-                                                    style="background-image: url('${json[i].image.url}')")></div>
-                                            </a>`;
-        }
-    }, 2000);
-}
 
 callApi();
